6e23173863b16c66da9dd2e0ee404d042ddde61f
HAMZALAPTOP\Hamza Zafar <humzazafar95@gmail.com>
2018-11-03 14:48:22 -0400

    -Routes -Login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Hamza routes
+
+
+//Pascal routes
